a9cc9ea84cbd3e5f6ebeeae1db0fd2da751fa3aa
Your Name Comes Here <archie808@hotmail.com>
2020-02-28 04:53:12 -0700

    Extra credit attempt.

---
+++ src/edu/cnm/deepdive/model/Shoe.java
@@ -0,0 +1,41 @@
+package edu.cnm.deepdive.model;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
+public class Shoe {
+
+  private List<Card> cards;
+  private Random rng;
+  private List<Card> dealt;
+
+  public Iterable<Deck> decks(Random rng) {
+    dealt = new LinkedList<>();
+    cards = new ArrayList<>();
+    for (Suit s : Suit.values()) {
+      for (Rank r : Rank.values()) {
+        cards.add(new Card(s, r));
+      }
+    }
+    while (cards != null) {
+      Card card = cards.isEmpty() ? null : cards.remove(0);
+      dealt.add(card);
+      dealt.clear();
+    }
+    return decks(rng);
+  }
+  public void shuffle() {
+    Collections.shuffle(cards, rng);
+  }
+
+  public Card deal() {
+    Card card = cards.isEmpty() ? null : cards.remove(0);
+    if (card != null) {
+      dealt.add(card);
+    }
+    return card;
+  }
+}
